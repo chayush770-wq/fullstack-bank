4c82ca5f530f3fdb8bd26adf09b0ca71d91cc475
Chaya Teitelbaum <chayush770@gmail.com>
2026-05-26 10:29:33 +0300

    fix gmail smtp connection in production

--- backend/services/email.service.js
+++ backend/services/email.service.js
@@ -3,12 +3,17 @@ const nodemailer = require("nodemailer");
 /* ===== MAIL TRANSPORT ===== */
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+
+  requireTLS: true,
+  family: 4,
 
   connectionTimeout: 10000,
   greetingTimeout: 10000,
